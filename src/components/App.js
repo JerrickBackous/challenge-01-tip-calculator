20c4958ac822ec7a416739684b659eee406e3da5
JerrickBackous <jerrick.backous20@gmail.com>
2023-08-15 23:30:38 -0500

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import Question from './Question';
+import Input from './Input';
+
+const serviceLevel = [
+  "Absolutely amazing! (20%)",
+  "It was good (15%)",
+  "It was fine (10%)",
+  "It was bad (5%)"
+];
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Question>How much was the bill?</Question>
+        <span><Input><input type='number'>
+                    </input></Input></span>
+      </div>
+      <div>
+        <Question>How did you like the service?</Question>
+        <span><Input>
+                    <select>
+            {serviceLevel.map(option =>
+                   <option value={option}>
+                    {option}
+                   </option>     
+                )}
+                    </select></Input></span>
+      </div>
+      <div>
+        <Question>How did your friend like the service?</Question>
+        <span><Input><select>
+            {serviceLevel.map(option =>
+                   <option value={option}>
+                    {option}
+                   </option>     
+                )}
+                    </select></Input></span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
